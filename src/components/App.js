5d93b69532b378f28bdd11c23d6abccfb1ba3f44
BAbdalwahed <b.abdalwahed@hotmail.com>
2023-03-02 20:13:56 +0000

    added .git ignore

--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@ import React from "react";
 import "../styles/App.css";
 import Myhashtags from "./MyHashtags";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <h1>Hashtag Generator</h1>
       <Myhashtags />
     </div>
   );
-}
+};
 
 export default App;
